933ac5c92599bfe6652f459067e40242c82beb5d
Julien Marcou <julien.marcou.dev@gmail.com>
2022-01-09 22:11:31 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ionic-focus-scroll-bug',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
